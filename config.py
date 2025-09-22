21b2606697c018dc1921360f3c1190168582a57c
Ahmed Elsayed <ahmed.malk2003.1.8@gmail..com>
2025-09-22 11:27:15 +0300

    change e5 to sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 # --- إعدادات ChromaDB ---
-MODEL_NAME = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
+MODEL_NAME = 'intfloat/multilingual-e5-large'
 PERSIST_DIRECTORY = "my_chroma_db"
 COLLECTION_NAME = "recruitment_qa"
 
